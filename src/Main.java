5d9d3076fc45949d8a5795299ece7faf6688cc9a
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-03-04 07:13:19 +0100

    add method readMonth

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
   enum Month {
     UNKNOWN, // неизвестный месяц
@@ -20,6 +24,19 @@ public class Main {
     SPRING, // ВЕСНА
     SUMMER, // ЛЕТО
     FALL, // ОСЕНЬ // AUTUMN - GB, FALL - US
+  }
+
+  public static Month readMonth() throws IOException {
+    BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+
+    System.out.print("Enter the month name: ");
+    String month = read.readLine();
+
+    try {
+      return Month.valueOf(month.toUpperCase());
+    } catch (IllegalArgumentException e) {
+      return Month.UNKNOWN;
+    }
   }
 
   public static void main(String[] args) {
