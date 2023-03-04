1e762c3870209745a3dae0c430827193353e0135
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-03-04 07:07:08 +0100

    add enums Month and Seasons

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,26 @@
-//  // Добавьте ещё один enum, который будет описывать времена года (зима, лето, весна, осень)
-//  // и укажет, к какому именно времени года относится выбранный месяц.
-
 public class Main {
+  enum Month {
+    UNKNOWN, // неизвестный месяц
+    JANUARY,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER,
+  }
+
+  enum Season { // ВремяГода
+    WINTER, // ЗИМА
+    SPRING, // ВЕСНА
+    SUMMER, // ЛЕТО
+    FALL, // ОСЕНЬ // AUTUMN - GB, FALL - US
+  }
 
   public static void main(String[] args) {
 
